773d56fd6eb05bf91e29fd59b48e741e09080731
CamAre0611 <camilita0611@gmail.com>
2025-05-15 18:56:19 -0500

    fix: corregido app.py con nombres correctos y paréntesis cerrado

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 app.register_blueprint(login_blueprint)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, de
+    app.run(host="0.0.0.0", port=5002, debug=True)
