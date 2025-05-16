b569d68f2efd29bea3b72748a4dbe1b77731ceb7
CamAre0611 <camilita0611@gmail.com>
2025-05-15 23:29:46 -0500

    Fix: se corrige login_service con validación de rol

--- service/login_service.py
+++ service/login_service.py
@@ -1,13 +1,13 @@
 class LoginService:
-    def validate_credentials(self, username, password):
+    def validate_credentials(self, username, password, rol):
         try:
             with open("usuarios.txt", "r") as file:
                 for line in file:
                     parts = line.strip().split(",")
-                    if len(parts) >= 2:
-                        user, pwd = parts[0], parts[1]
-                        print(f"Comparando: input({username},{password}) vs archivo({user},{pwd})")
-                        if user == username and pwd == password:
+                    if len(parts) >= 3:
+                        user, pwd, user_rol = parts[0], parts[1], parts[2]
+                        print(f"Comparando: input({username}, {password}, {rol}) vs archivo({user}, {pwd}, {user_rol})")
+                        if user == username and pwd == password and rol == user_rol:
                             return True
             return False
         except Exception as e:
